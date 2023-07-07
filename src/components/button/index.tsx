fa1886dad06155aab696dfd4be629fbfbab5e894
0214kbs <0214kbs@naver.com>
2023-07-07 10:53:52 +0900

    feat: eslint, prettier 적용

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,13 +3,11 @@ import Button from "./Button";
 import { ButtonProps } from "./button.type";
 
 const ButtonComponent = ({ primary = false, size = "medium", bgcolor, label, ...props }: ButtonProps) => {
-    return (
-        <>
-          <Button primary={primary} size={size} bgcolor={bgcolor}>
-            {label}
-          </Button>
-        </>
-      )
+  return (
+    <>
+      <Button primary={primary} size={size} bgcolor={bgcolor} label={label} />
+    </>
+  )
 };
 
 export default ButtonComponent;
