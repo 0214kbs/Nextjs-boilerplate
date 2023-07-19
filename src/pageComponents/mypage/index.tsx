e230c5aa2aba3778aa2aeba72be61256f0dde5ab
0214kbs <0214kbs@naver.com>
2023-07-20 01:25:01 +0900

    feat: mypage 회원정보 불러오기

--- src/pageComponents/mypage/index.tsx
+++ src/pageComponents/mypage/index.tsx
@@ -1,14 +1,51 @@
 "use client";
 import React from "react";
+import useLoginStore from "@/stores/useLoginStore";
 
 const Mypage = () => {
-    return (
-        <>
-            <div>
-                <h2> MyPage </h2>
-            </div >
-        </>
-    );
+  const { user } = useLoginStore();
+
+  return (
+    <>
+      <div>
+        <h2> MyPage </h2>
+        <table>
+          <thead>
+            <tr>
+              <th>index</th>
+              <th>value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>ID</td>
+              <td>{user.id}</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{user.email}</td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>{user.name}</td>
+            </tr>
+            <tr>
+              <td>Birth</td>
+              <td>{user.birth}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{user.gender}</td>
+            </tr>
+            <tr>
+              <td>Phone</td>
+              <td>{user.phone}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default Mypage;
